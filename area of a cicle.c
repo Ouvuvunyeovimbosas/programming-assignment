fcdbbadc8123e545ab72cf991714b6bc74bdfcc3
Ouvuvunyeovimbosas <102598265+Ouvuvunyeovimbosas@users.noreply.github.com>
2022-03-30 15:47:05 +0300

    Add files via upload

---
+++ area of a cicle.c
@@ -0,0 +1,11 @@
+//program to find the area of a circle
+#include <stdio.h>
+int main()
+{
+    int radius=21;
+    int area;
+    area=22/7*21*21;
+    printf("the area of the circle with radius:%d is:%d sq. units",radius,area);
+    return 0;
+}
+
