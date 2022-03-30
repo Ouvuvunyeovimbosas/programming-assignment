3a94e01f5a00b31c839c6300893b3ce229fcc98f
Ouvuvunyeovimbosas <102598265+Ouvuvunyeovimbosas@users.noreply.github.com>
2022-03-30 15:45:39 +0300

    Add files via upload

---
+++ sum of 5 nos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a=20,b=7,c=9,d=12,e=15,sum; /*Variable Defining and Assign values */
+    sum=a+b+c+d+e;
+    printf("sum of five numbers is %d\n", sum);
+    return 0;
+
+}
